2b12ae6cd619a4933e44567974c869c2b38825b0
Bezaleel Ashefor <3107111+knightbenax@users.noreply.github.com>
2024-03-07 09:30:51 +0100

    Expired Transfer Views

--- wraith/src/main/java/com/nomba/wraith/ui/shelters/transfer/TransferExpiredShelter.kt
+++ wraith/src/main/java/com/nomba/wraith/ui/shelters/transfer/TransferExpiredShelter.kt
@@ -3,5 +3,6 @@ package com.nomba.wraith.ui.shelters.transfer
 import com.nomba.wraith.core.Shelter
 import com.nomba.wraith.databinding.TransferViewBinding
 
+
 class TransferExpiredShelter(activityTransferViewBinding: TransferViewBinding) : Shelter(activityTransferViewBinding) {
 }
